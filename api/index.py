fc4c80bb9bf96f08198384c281b4b097ff96c305
tison <wander4096@gmail.com>
2022-08-24 23:55:25 +0800

    demo python functions
    Signed-off-by: tison <wander4096@gmail.com>

---
+++ api/index.py
@@ -0,0 +1,13 @@
+from http.server import BaseHTTPRequestHandler
+from cowpy import cow
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        message = cow.Cowacter().milk('Hello from Python from a Serverless Function!')
+        self.wfile.write(message.encode())
+        return
+
