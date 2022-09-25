376d9a8c6b1d6ee0288a03e5d974c44039b77a5f
tison <wander4096@gmail.com>
2022-09-25 11:36:46 +0800

    feat: Giscus as comment system
    Signed-off-by: tison <wander4096@gmail.com>

---
+++ src/components/Comments/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Giscus from "@giscus/react";
+
+export default function Comments({discussionId}): JSX.Element {
+    return (
+        <Giscus
+            id="comments"
+            repo="korandoru/open-source-town"
+            repoId="R_kgDOH0siXw"
+            mapping="number"
+            term={discussionId}
+            reactionsEnabled="1"
+            emitMetadata="0"
+            inputPosition="top"
+            theme="light"
+            lang="zh-CN"
+            loading="lazy"
+        />
+    );
+}
