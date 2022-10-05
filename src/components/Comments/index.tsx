9e903a59f29a35cf741102f2260d7e11c455d084
tison <wander4096@gmail.com>
2022-10-05 22:38:11 +0800

    feat: switch comments theme on color mode changed
    Signed-off-by: tison <wander4096@gmail.com>

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -1,20 +1,32 @@
 import React from 'react'
 import Giscus from '@giscus/react'
+import {useColorMode} from '@docusaurus/theme-common'
+import {Theme} from '@giscus/react/dist/types'
+
+function themeFromColorMode(): Theme {
+  const {colorMode} = useColorMode()
+  switch (colorMode) {
+    case 'light':
+      return "light"
+    case 'dark':
+      return "dark"
+  }
+}
 
 export default function Comments({discussionId}): JSX.Element {
   return (
     <Giscus
-      id="comments"
-      repo="korandoru/open-source-town"
-      repoId="R_kgDOH0siXw"
-      mapping="number"
+      id='comments'
+      repo='korandoru/open-source-town'
+      repoId='R_kgDOH0siXw'
+      mapping='number'
       term={discussionId}
-      reactionsEnabled="1"
-      emitMetadata="0"
-      inputPosition="top"
-      theme="light"
-      lang="zh-CN"
-      loading="lazy"
+      reactionsEnabled='1'
+      emitMetadata='0'
+      inputPosition='top'
+      theme={themeFromColorMode()}
+      lang='zh-CN'
+      loading='lazy'
     />
   )
 }
