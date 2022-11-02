eb510dfe3e32673eab2ba7756dbb197f1539455c
tison <wander4096@gmail.com>
2022-11-02 09:40:45 +0800

    fix: use document.title after page load
    This is the same solution as
    https://github.com/facebook/docusaurus/pull/7424.
    
    Signed-off-by: tison <wander4096@gmail.com>

--- globalClientModule.ts
+++ globalClientModule.ts
@@ -5,11 +5,11 @@ const module: ClientModule = {
   onRouteDidUpdate({location, previousLocation}) {
     if (ExecutionEnvironment.canUseDOM) {
       if (location.pathname != previousLocation?.pathname) {
-        _paq.push(['setCustomUrl', location.pathname])
-        _paq.push(['setDocumentTitle', document.title])
-        _paq.push(['trackPageView'])
-        console.log(`path = ${location.pathname}`)
-        console.log(`title = ${document.title}`)
+        setTimeout(() => {
+          _paq.push(['setCustomUrl', location.pathname])
+          _paq.push(['setDocumentTitle', document.title])
+          _paq.push(['trackPageView'])
+        })
       }
     }
   }
