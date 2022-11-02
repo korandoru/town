c298186b05f266d4cf2605b67ee6a6f674b41cbd
tison <wander4096@gmail.com>
2022-11-02 09:49:31 +0800

    chore: set setReferrerUrl if possible
    Signed-off-by: tison <wander4096@gmail.com>

--- globalClientModule.ts
+++ globalClientModule.ts
@@ -6,6 +6,9 @@ const module: ClientModule = {
     if (ExecutionEnvironment.canUseDOM) {
       if (location.pathname != previousLocation?.pathname) {
         setTimeout(() => {
+          if (previousLocation) {
+            _paq.push(['setReferrerUrl', previousLocation.pathname])
+          }
           _paq.push(['setCustomUrl', location.pathname])
           _paq.push(['setDocumentTitle', document.title])
           _paq.push(['trackPageView'])
